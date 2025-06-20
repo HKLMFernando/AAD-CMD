072ea0beaa43cc6da897b3ba689a95969c6fbd52
HKLMFernando <matheeshafernando61@gmail.com>
2025-06-20 10:43:37 +0530

    update

---
+++ src/main/java/com/assignment/controller/ComplaintServlet.java
@@ -0,0 +1,21 @@
+package com.assignment.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+
+public class ComplaintServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
